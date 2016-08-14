9c1214dc7a8f9c1c1ea3d001f1ff1ddb7bbb8f05
Ben Moon <guiltydolphin@gmail.com>
2016-08-14 12:21:57 +0100

    Add additional map helpers

--- Util.hs
+++ Util.hs
@@ -2,18 +2,22 @@ module Util
   ( mapElapsedTime
   , slowTrack
   , speedTrack
+  , mapTrackVoice
+  , mapPitch
   ) where
 
 
 import qualified Sound.MIDI.File as F
+import qualified Sound.MIDI.File.Event as E
 import qualified Data.EventList.Relative.TimeBody as T
 import qualified Sound.MIDI.Message.Channel.Voice as V
+import qualified Sound.MIDI.Message.Channel as C
 
 
 type MIDI = F.T
-
-
 type Voice = V.T
+type MEvent = E.T
+type MessageChan = C.T
 
 
 mapElapsedTime :: (Integer -> Integer) -> MIDI -> MIDI
@@ -39,3 +43,16 @@ mapVelocity :: (V.Velocity -> V.Velocity) -> Voice -> Voice
 mapVelocity f (V.NoteOff p v) = V.NoteOff p (f v)
 mapVelocity f (V.NoteOn  p v) = V.NoteOn  p (f v)
 mapVelocity _ v = v
+
+
+mapEvent :: (MEvent -> MEvent) -> F.Track -> F.Track
+mapEvent = T.mapBody
+
+
+mapMidiEvent :: (MessageChan -> MessageChan) -> MEvent -> MEvent
+mapMidiEvent f (E.MIDIEvent e) = E.MIDIEvent (f e)
+mapMidiEvent _ e = e
+
+
+mapTrackVoice :: (Voice -> Voice) -> F.Track -> F.Track
+mapTrackVoice = mapEvent . E.mapVoice
