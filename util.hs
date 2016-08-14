7ce2470153262df751aa3db0793ffa70077d256f
Ben Moon <guiltydolphin@gmail.com>
2016-08-14 11:56:19 +0100

    Add helpers for mapping pitch and velocity

--- util.hs
+++ util.hs
@@ -7,9 +7,13 @@ module Util
 
 import qualified Sound.MIDI.File as F
 import qualified Data.EventList.Relative.TimeBody as T
+import qualified Sound.MIDI.Message.Channel.Voice as V
 
 
 type MIDI = F.T
+
+
+type Voice = V.T
 
 
 mapElapsedTime :: (Integer -> Integer) -> MIDI -> MIDI
@@ -22,3 +26,16 @@ slowTrack n = mapElapsedTime (*n)
 
 speedTrack :: Integer -> MIDI -> MIDI
 speedTrack n = mapElapsedTime (`div`n)
+
+
+mapPitch :: (V.Pitch -> V.Pitch) -> Voice -> Voice
+mapPitch f (V.NoteOff p v) = V.NoteOff (f p) v
+mapPitch f (V.NoteOn  p v) = V.NoteOn  (f p) v
+mapPitch f (V.PolyAftertouch p pr) = V.PolyAftertouch (f p) pr
+mapPitch _ v = v
+
+
+mapVelocity :: (V.Velocity -> V.Velocity) -> Voice -> Voice
+mapVelocity f (V.NoteOff p v) = V.NoteOff p (f v)
+mapVelocity f (V.NoteOn  p v) = V.NoteOn  p (f v)
+mapVelocity _ v = v
