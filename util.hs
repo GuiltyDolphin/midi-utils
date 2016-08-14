7400c4b460d0fbc3cf0794ad8c94f88c968aa460
Ben Moon <guiltydolphin@gmail.com>
2016-08-14 10:22:39 +0100

    Add alias for Sound.MIDI.File.T (to MIDI)

--- util.hs
+++ util.hs
@@ -8,11 +8,17 @@ module Util
 import qualified Sound.MIDI.File as F
 import qualified Data.EventList.Relative.TimeBody as T
 
-mapElapsedTime :: (Integer -> Integer) -> F.T -> F.T
+
+type MIDI = F.T
+
+
+mapElapsedTime :: (Integer -> Integer) -> MIDI -> MIDI
 mapElapsedTime f = F.mapTrack (T.mapTime (F.toElapsedTime . f . F.fromElapsedTime))
 
-slowTrack :: Integer -> F.T -> F.T
+
+slowTrack :: Integer -> MIDI -> MIDI
 slowTrack n = mapElapsedTime (*n)
 
-speedTrack :: Integer -> F.T -> F.T
+
+speedTrack :: Integer -> MIDI -> MIDI
 speedTrack n = mapElapsedTime (`div`n)
