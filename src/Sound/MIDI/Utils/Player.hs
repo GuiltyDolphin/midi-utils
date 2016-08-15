5fc039aea5cb98b9ccda1503bf3d2457b423d8ce
Ben Moon <guiltydolphin@gmail.com>
2016-08-15 16:23:39 +0100

    Add classes for Stream and File Players

--- src/Sound/MIDI/Utils/Player.hs
+++ src/Sound/MIDI/Utils/Player.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Sound.MIDI.Utils.Player
   ( Player(..)
+  , PlayerStream(..)
+  , PlayerFile(..)
   ) where
 
 
@@ -11,6 +13,19 @@ import qualified Sound.MIDI.File as F
 type MIDI = F.T
 
 
+-- | Players that can play MIDI on a port.
 class Player player port | player -> port where
+  -- | Use a port to construct a @player@.
   makePlayer :: port -> player
-  playMidi   :: player -> MIDI -> IO ()
+
+
+-- | Players that can directly play MIDI data.
+class PlayerStream player where
+  -- | Play the given 'MIDI' data to the port.
+  playMidi :: player -> MIDI -> IO ()
+
+
+-- | Players that can directly play a MIDI file.
+class PlayerFile player where
+  -- | Play a 'MIDI' file to the port.
+  playMidiFile :: player -> FilePath -> IO ()
